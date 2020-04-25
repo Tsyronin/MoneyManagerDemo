a8c1c16a555a6ae83964cb133f4eaeb3c99f1fb7
Pavlo <pavelcrn11@gmail.com>
2020-04-26 00:40:47 +0300

    Added functionality to create and edit records with a list of tags.

---
+++ MoneyManagerUI/ViewModel/RecordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace MoneyManagerUI.ViewModel
+{
+    public class RecordViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Sum { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int SubcategoryId { get; set; }
+
+        public int[] TagIds { get; set; }
+        public MultiSelectList Tags { get; set; }
+    }
+}
